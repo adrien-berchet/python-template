1b543af554c2a98ebdb9dc502d19420486603077
genrich <genrich.ivaska@epfl.ch>
2019-04-11 15:10:07 +0200

    add official sphinx theme, bump python 3 supported version
    Change-Id: I10b55168d6d2cc1b880c6d4371f2be316b18517c

--- {{cookiecutter.project_name}}/doc/source/conf.py
+++ {{cookiecutter.project_name}}/doc/source/conf.py
@@ -43,7 +43,6 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'{{ cookiecutter.project_name }}'
-copyright = u'{{ cookiecutter.year }}, Blue Brain Project'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -93,7 +92,8 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'haiku'
+html_theme = 'sphinx-limestone-theme'
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -105,7 +105,7 @@ html_theme = 'haiku'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = u'{{ cookiecutter.project_name }}'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
