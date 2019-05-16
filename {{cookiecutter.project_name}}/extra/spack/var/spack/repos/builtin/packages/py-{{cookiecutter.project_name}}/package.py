261150b4a5f4c7c9fedb380dcf4dca0eb68dcd46
Arseny V. Povolotsky <arseny.povolotsky@epfl.ch>
2019-05-16 10:10:50 +0200

    Removed Nix-related templates
     * added Spack recipe template instead
    
    Change-Id: Ida448b9891cad565e96790ec6c85bc26e8d0acf5

---
+++ {{cookiecutter.project_name}}/extra/spack/var/spack/repos/builtin/packages/py-{{cookiecutter.project_name}}/package.py
@@ -0,0 +1,22 @@
+# Copyright 2013-2018 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+# replace all 'x-y' with 'xY' (e.g. 'Py-morph-tool' -> 'PyMorphTool')
+class Py-{{ cookiecutter.project_name }}(PythonPackage):
+    """{{ cookiecutter.description }}"""
+
+    homepage = "{{ cookiecutter.project_url }}"
+    git      = "{{ cookiecutter.download_url }}"
+
+    version('develop', branch='master')
+    version('{{ cookiecutter.version }}', tag='{{ cookiecutter.project_name }}-v{{ cookiecutter.version }}', preferred=True)
+
+    depends_on('py-setuptools', type='build')  # type=('build', 'run') if specifying entry points in 'setup.py'
+
+    # for all 'foo>=X' in 'install_requires' and 'extra_requires':
+    # depends_on('py-foo@<min>:')
